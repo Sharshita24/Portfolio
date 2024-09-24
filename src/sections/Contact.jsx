03ce98124564206545cd890fb7e9d58773f533b7
Harshita Saini <119687415+Sharshita24@users.noreply.github.com>
2024-09-25 00:31:48 +0530

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -21,8 +21,7 @@ const Contact = () => {
     setLoading(true);
 
     emailjs
-      .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      .send('service_9dg4ln4',
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
@@ -31,7 +30,7 @@ const Contact = () => {
           to_email: 'harshita.saini2411@gmail.com',
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY,
+        'g9I2UhGLvm97x1Z2S'
       )
       .then(
         () => {
