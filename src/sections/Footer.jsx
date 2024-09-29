2bc28a2d96956935b2405cc5e180c358ce5b57d5
Harshita Saini <119687415+Sharshita24@users.noreply.github.com>
2024-09-29 23:35:04 +0530

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,9 +2,7 @@ const Footer = () => {
     return (
       <footer className="c-space pt-7 pb-3 border-t border-black-300 flex justify-between items-center flex-wrap gap-5">
         <div className="text-white-500 flex gap-2">
-          <p>Terms & Conditions</p>
-          <p>|</p>
-          <p>Privacy Policy</p>
+          
         </div>
   
         <div className="flex gap-3">
